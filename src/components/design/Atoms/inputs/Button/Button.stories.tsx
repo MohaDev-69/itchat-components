69b168064849d034f810c2c6a11b5e725018c248
z7pz <sdfygamer@gmail.com>
2022-07-09 16:30:25 +0300

    fix: bug fixes

--- src/components/design/Atoms/inputs/Button/Button.stories.tsx
+++ src/components/design/Atoms/inputs/Button/Button.stories.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import { ButtonComponent } from '.';
+import { ButtonComponent } from ".";
 
 export default {
-    title: 'design/Atoms/inputs/Button',
-    component: ButtonComponent,
-} as ComponentMeta<typeof ButtonComponent>
+  title: "design/Atoms/inputs/Button",
+  component: ButtonComponent,
+} as ComponentMeta<typeof ButtonComponent>;
 
-const Template: ComponentStory<typeof ButtonComponent> = (args: any) => <ButtonComponent {...args} />;
+const Template: ComponentStory<typeof ButtonComponent> = (args: any) => (
+  <ButtonComponent {...args} />
+);
 
 export const Default = Template.bind({});
 
-Default.args ={
-    "text": "Button"
-}
+Default.args = {
+  text: "Button",
+  size: "md",
+};
--- src/components/design/Atoms/inputs/Button/Button.tsx
+++ src/components/design/Atoms/inputs/Button/Button.tsx
@@ -2,33 +2,24 @@ import React from "react";
 
 import { Button, MantineSize } from "@mantine/core";
 export interface IButtonProps {
-  href: string;
-  newTab: boolean;
-  size: MantineSize;
-  text: string;
-  color: string;
-  rightIcon: string;
-  leftIcon: string;
-  onClick: (event: React.MouseEvent<HTMLElement>) => void;
+  size?: MantineSize;
+  text?: string;
+  color?: string;
+  rightIcon?: string;
+  leftIcon?: string;
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
 }
 export const ButtonComponent = (props: IButtonProps) => {
-  const { size, href, newTab, text, rightIcon, leftIcon, color, onClick } =
-    props;
-  const [NewTab, SetNewTab] = React.useState("");
-  React.useEffect(() => {
-    newTab == true ? SetNewTab("_blank") : SetNewTab("");
-  }, [newTab]);
+  const { size = "md", text, rightIcon, leftIcon, color, onClick } = props;
   return (
-    <a href={href} target={NewTab}>
-      <Button
-        size={size}
-        rightIcon={rightIcon}
-        leftIcon={leftIcon}
-        style={{ backgroundColor: color }}
-        onClick={onClick}
-      >
-        {text}
-      </Button>
-    </a>
+    <Button
+      size={size}
+      rightIcon={rightIcon}
+      leftIcon={leftIcon}
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {text}
+    </Button>
   );
 };
